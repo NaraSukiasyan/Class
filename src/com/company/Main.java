70f46fbd1e96679ee35cf123d56946bcf3d8e823
newuser <narasukiasyanb93@mail.ru>
2022-04-07 11:48:13 +0400

    Class Box

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+class Box{
+    double width;
+    double height;
+    double depth;
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+	Box myBox = new Box();
+    double vol;
+    myBox.width = 10;
+    myBox.height = 20;
+    myBox.depth = 15;
+    vol = myBox.width * myBox.height * myBox.depth;
+        System.out.println("vol is" + vol);
+    }
+}
